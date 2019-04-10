dbd56ed3221dd2feded1d7046e0a8041e3d10278
Molly Nelson <mollykathrynnelson@gmail.com>
2019-04-10 02:33:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ function calculatesFarePrice(start, destination){
       return 0;
     } else if (distance > 400 && distance <= 2000){
       return 0.02 * (distance - 400);
-    } else if (distance < 2000 && distance < 2500){
+    } else if (distance > 2000 && distance < 2500){
     return 25;
   } else {
     return "cannot travel that far";
